9cd810198acae5b6bfe8c7c8157c0a378a06d583
Viet Vu <jooservices@gmail.com>
2024-06-10 20:29:24 +0700

    update bash

--- speedtest.sh
+++ speedtest.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ssh gatekeeper "speedtest --format=json 2>&1" > ./speedtest.json
-curl -F "file=@./speedtest.json" http://127.0.0.1:8000/api/v1/speedtest
-
-#unlink ./speedtest.json
+ssh $1 "speedtest --format=json 2>&1" > ./$1.json
+curl -F "file=@./${1}.json" http://127.0.0.1:8000/api/v1/speedtest
+unlink ./$1.json
